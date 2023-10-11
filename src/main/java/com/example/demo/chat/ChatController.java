9df2296d8a10f1282a0b09d9d396390d1bf45d20
hung <nvh202sua@gmail.com>
2023-10-11 16:20:08 +0700

    single room

--- src/main/java/com/example/demo/chat/ChatController.java
+++ src/main/java/com/example/demo/chat/ChatController.java
@@ -3,6 +3,7 @@ package com.example.demo.chat;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -11,16 +12,16 @@ import org.springframework.stereotype.Controller;
 @AllArgsConstructor
 public class ChatController {
 
-    private final SimpMessageSendingOperations messageTemplate;
+    // private final SimpMessageSendingOperations messageTemplate;
 
     @MessageMapping("/chat.sendMessage")
-    // @SendTo("/topic/public")
+    @SendTo("/topic/public")
     public ChatClient sendMessage(@Payload ChatClient chatClient) {
         return chatClient;
     }
 
     @MessageMapping("/chat.addUser")
-    // @SendTo("/topic/public")
+    @SendTo("/topic/public")
     public ChatClient addUser(
             @Payload ChatClient chatClient,
             SimpMessageHeaderAccessor headerAccessor
